1b89918e7f58417c10e17abcd053ebde455eb336
Renato Castañeda Carrillo <renato_cc2@hotmail.com>
2015-11-09 01:09:49 -0600

    1er test de unidad y mapeo de url

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import pagina_inicio #1
 
-class HomePageTest(TestCase):
+class PruebaPaginaInicio(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')  #2
