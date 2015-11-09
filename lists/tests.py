bb11d01fd9451363bde79c714f4661a2bd33ddb9
Renato Castañeda Carrillo <renato_cc2@hotmail.com>
2015-11-09 01:08:06 -0600

    1er test de unidad y mapeo de url

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import pagina_inicio #1
 
-class AlcoholTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(3 + 3, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  #2
+        self.assertEqual(found.func, pagina_inicio)  #3
